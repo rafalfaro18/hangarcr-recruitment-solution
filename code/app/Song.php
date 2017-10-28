0a4ce21853d243b0f70802168e5da24cc7544bb0
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-10-27 21:24:04 -0600

    no handling of timestamps

--- code/app/Song.php
+++ code/app/Song.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    //
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'songs';
+  public $timestamps = false;
 }
