71c3961f63a303a85c6ed94a3dff6f1d274a2995
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-10-29 20:04:32 -0600

    beutified Song.php

--- code/app/Song.php
+++ code/app/Song.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-  /**
-   * The table associated with the model.
-   *
-   * @var string
-   */
-  protected $table = 'songs';
-  public $timestamps = false;
-  protected $fillable = ['id', 'url', 'songname', 'artistid', 'artistname', 'albumid', 'albumname'];
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'songs';
+    public $timestamps = false;
+    protected $fillable = ['id', 'url', 'songname', 'artistid', 'artistname', 'albumid', 'albumname'];
 }
